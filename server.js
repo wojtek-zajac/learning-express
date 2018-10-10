3351c4f04ec33b9d0fe4475b63b33a4f16a49c2e
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-10-10 21:28:43 +0200

    Fix issue with nesting user items and remove enpoints returning json

--- server.js
+++ server.js
@@ -1,32 +1,19 @@
 const express = require('express')
 const app = express()
 const getUserById = require('./users/getById')
-const getUsers = require('./users/getUsers')
-const usersJson = require('./users/users')
+const getUsers = require('./users/get')
 
 app.set('views', './views')
-
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Best website ever', message: 'Welcome!'})
 })
 
-app.get('/api/users', (req, res) => {
-    res.status(200).json(getUsers())
-})
-
 app.get('/users', (req, res) => {
-    res.status(200).render('users', {title: 'All users', usersJson})
-})
-
-app.get('/api/users/:id', (req, res) => {
-    res.status(200).json(getUserById(req.params.id))
+    res.status(200).render('users', {title: 'All users', users: getUsers()})
 })
 
 const server = app.listen(8080, () => {
     console.log(`App is running on port ${server.address().port}`)
 })
-
-// GET /users - funkcja get w folderze users zwracająca wszystkich userów
-// dopisz endpoint GET /users - HTML zawierający listę userów (iteracja w pugu)
