665a0ffdcf48420427c4244574b62cb2bde5dcc9
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-10-10 23:54:44 +0200

    Improve user routing

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/users', (req, res) => {
     res.status(200).render('users', {title: 'All users', users: getUsers()})
 })
 
+app.get('/users/:id', (req, res) => {
+    res.status(200).render('user', {name: 'User Name', users: getUserById(req.params.id)})
+})
+
 const server = app.listen(8080, () => {
     console.log(`App is running on port ${server.address().port}`)
 })
