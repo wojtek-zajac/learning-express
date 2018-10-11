11c9ed947aa06318285fa3613b44c235624a18a5
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-10-11 15:09:01 +0200

    Improve serving static files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+
 const getUserById = require('./users/getById')
 const getUsers = require('./users/get')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
-app.use('/styles', express.static(__dirname + '/styles'));
+
+app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Best website ever', message: 'Welcome!'})
@@ -16,7 +18,8 @@ app.get('/users', (req, res) => {
 })
 
 app.get('/users/:id', (req, res) => {
-    res.status(200).render('users', {users: getUsers(), user: getUserById(req.params.id)})
+    const user = getUserById(req.params.id)
+    res.status(200).render('users', {title: user.name, users: getUsers(), user})
 })
 
 const server = app.listen(8080, () => {
